86d06309d8d29310ca00b6016c2fd5c19d63f369
Zhijian Duan <zjduan1998@gmail.com>
2018-06-30 21:40:58 +0800

    modified by dzj

--- Source/densenet.py
+++ Source/densenet.py
@@ -6,11 +6,12 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
-from MURAdataset import _load_model, clip, Focal_Loss, MURAdataset, per_class_dataset, sigmoid_test, \
-    save_model
+from MURAdataset import _load_model, clip, Focal_Loss, MURAdataset, per_class_dataset, sigmoid_test
 import os
 import sys
 import argparse
+import time
+
 
 def str2bool(v):
     if v.lower() in ['yes', 'true', 't', 'y', '1']:
@@ -19,6 +20,7 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Unsupported value encountered.')
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -32,20 +34,15 @@ def parse_args():
     parser.add_argument('--save_path', type=str)
     return parser.parse_args()
 
-def model_modify(net, channels1, channels2):
-    conv0 = net.features[0]
-    tensor = conv0.weight
-    tensor = torch.chunk(tensor, 3, 1)
-    tensor = tensor[0]
-    newconv = torch.nn.Conv2d(1, channels1, 7, 2, 3)
-    newconv.weight = nn.Parameter(tensor)
-    net.features[0] = newconv
-    net.classifier = nn.Linear(channels2, 1)
-    return net
+
+# def model_modify(net, channels1, channels2):
+#     net.classifier = nn.Linear(channels2, 1)
+#     return net
+
 
 if __name__ == '__main__':
     args = parse_args()
-    for k,v in vars(args).items():
+    for k, v in vars(args).items():
         print k, ":", v
 
     batch_size = args.batch_size
@@ -59,69 +56,74 @@ if __name__ == '__main__':
     save_path = args.save_path
     if os.path.exists(save_path) == False:
         os.makedirs(save_path)
-    save_path += net_name
+    save_path += net_name + '.pkl'
 
-    train_transform = transforms.Compose([transforms.Resize((224, 224)),
+    train_transform = transforms.Compose([transforms.Resize([320, 320]),
+                                          transforms.CenterCrop(224),
                                           transforms.RandomHorizontalFlip(),
-                                          transforms.RandomRotation(10),
+                                          transforms.RandomRotation(30),
                                           transforms.ToTensor(),
-                                          transforms.Normalize((0.485,), (0.229,)),
+                                          transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                                           ])
-    test_transform = transforms.Compose([transforms.Resize((224, 224)),
+    test_transform = transforms.Compose([transforms.Resize((320, 320)),
+                                         transforms.CenterCrop(224),
                                          transforms.ToTensor(),
-                                         transforms.Normalize((0.485,), (0.229,)),
+                                         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                                          ])
 
     present_dir_path = os.getcwd()
 
     trainset = per_class_dataset(csv_file=r'/train.csv', root_dir=present_dir_path,
-                                 transform=train_transform, RGB=False)
+                                 transform=train_transform, RGB=True)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=4)
     testset = per_class_dataset(csv_file='/valid.csv', root_dir=present_dir_path,
-                                transform=test_transform, RGB=False)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=1,
+                                transform=test_transform, RGB=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=1)
 
-    net = torchvision.models.densenet169(pretrained=not load_model, drop_rate=drop_rate)
-    # net.classifier = nn.Linear(1664,1)
-    net = model_modify(net, 64, 1664)
+    net = torchvision.models.densenet169(pretrained=not load_model)
+    net.classifier = nn.Linear(1664, 1)
+    # net = model_modify(net,64,1664,Sigmoid=True)
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=20, verbose=True)
 
-    if torch.cuda.device_count() > 1:
-        print("Let's use %d GPUS!" % torch.cuda.device_count())
-        net = nn.DataParallel(net)
+    '''if torch.cuda.device_count() > 1:
+      print("Let's use GPUS!")
+       net = nn.DataParallel(net,device_ids=[0,2,3,4,5])
+    '''
 
     if load_model:
-        net = _load_model(net, torch.load(load_path))
+        print ("load from %s" % load_path)
+        net.load_state_dict(torch.load(load_path))
 
     if torch.cuda.is_available():
         net = net.cuda()
 
-    best_acc = 0
-    best_model_wts = copy.deepcopy(net.state_dict())
+    best_score = 0
 
     for step in range(epoch):
         epoch_loss = 0.0
         running_loss = 0.0
         total = 0
+        net.train()
         for i, data in enumerate(trainloader, 0):
 
             total += 1
 
             inputs, labels, weights = data
-            weights = weights.type(torch.DoubleTensor)
-            labels = labels.type(torch.DoubleTensor)
+            weights = weights.type(torch.FloatTensor)
+            labels = labels.type(torch.FloatTensor)
             inputs, labels, weights = Variable(inputs.cuda()), Variable(labels.cuda()), Variable(weights.cuda())
             optimizer.zero_grad()
             outputs = net(inputs).double()
-            outputs = torch.sigmoid(outputs)
+            outputs = torch.sigmoid(net(inputs))
             outputs = outputs.select(1, 0)
             outputs = torch.clamp(outputs, min=1e-7, max=1 - 1e-7)
             loss = -((1 - outputs) * labels * outputs.log() + outputs * (1 - labels) * (1 - outputs).log())
-            loss = (loss * weights).sum()
 
+            loss = (loss * weights).sum()
+            # loss = loss.sum()
             loss.backward()
             optimizer.step()
 
@@ -129,22 +131,26 @@ if __name__ == '__main__':
             epoch_loss += loss.data.item()
             if i % 500 == 499:
                 print('[%d, %5d] loss: %.3f' % (step + 1, i + 1, running_loss / 500))
-                # sys.stdout.flush()
+                sys.stdout.flush()
                 running_loss = 0.0
+        if step % 1 == 0:
+            net.eval()
+            test_score, test_acc, test_loss = sigmoid_test(net, testloader)
+            # scheduler.step(test_loss)
+            print('test_score, test_accuracy and loss in epoch %d : %.3f %.3f %.3f' % (step, test_score, test_acc, test_loss))
+            # print('epoch_loss in epoch %d : %.3f' % (step, epoch_loss / total))
+            sys.stdout.flush()
+            if test_score > best_score:
+                best_score = test_score
+                torch.save(net, save_path)
 
-        test_acc, test_loss = sigmoid_test(net, testloader)
-        # scheduler.step(test_loss)
-        print('test_accuracy and loss in epoch %d : %.3f %.3f' % (step, test_acc, test_loss))
-        # sys.stdout.flush()
-        if best_acc <= test_acc:
-            best_acc = test_acc
-            best_model_wts = copy.deepcopy(net.state_dict())
-
-    print ("start to test")
+    print('Finished training and start to test')
+    if best_score > 0:
+        print ("load from best score model")
+        net = _load_model(net, torch.load(save_path))
+    net.eval()
+    # test_score, test_acc, test_loss = sigmoid_test(net, testloader)
+    test_score = 0
     test_acc, test_loss = sigmoid_test(net, testloader)
-    print('test_acc and loss: %.3f %.3f' % (test_acc, test_loss))
-    net.load_state_dict(best_model_wts)
-    print('Finished Training')
-    save_model(net, save_path)
+    print('final test_score, test_accuracy and loss: %.3f %.3f %.3f' % (test_score, test_acc, test_loss))
     print('Finished saving')
-
--- Source/train.sh
+++ Source/train.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 net='densenet169'
-net_name='densenet169_b8_lr1e-4_d0'
-CUDA_VISIBLE_DEVICES=0,2,3,4,5,6 python -u densenet.py \
-    --batch_size 8 --epoch 40 --learning_rate 0.0001 --drop_rate 0 \
-    --load_model False  \
+net_name='b8_lr1e-4_d0_crop'
+CUDA_VISIBLE_DEVICES=5 python -u densenet.py \
+    --batch_size 8 --epoch 0 --learning_rate 0.0001 --drop_rate 0 \
+    --load_model True --load_path ./save/$net/mura3crop_1_loss.pkl \
     --net_name $net_name \
-    --save_path save/$net/ \
-    > result/$net/$net_name.out
+    --save_path ./save/$net/ \
+    > result/$net/${net_name}_+1.out
