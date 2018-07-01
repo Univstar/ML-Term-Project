96ffc620f889449b116d200cb55ae9fadb3d86c7
Zhijian Duan <zjduan1998@gmail.com>
2018-07-01 13:14:53 +0800

    delete something

--- Source/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-net='densenet169'
-net_name='baseline'
-CUDA_VISIBLE_DEVICES=2 python -u densenet.py \
-    --batch_size 8 --epoch 0 --learning_rate 0.0001 --drop_rate 0 \
-    --load_model False \
-    --net_name $net_name \
-    --save_path ./save/$net/ \
-    > output.out
