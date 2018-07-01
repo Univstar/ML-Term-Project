11d829999ecdb8704643a543020e9efc1bbc3477
Univstar <nixy@qq.com>
2018-07-01 12:43:24 +0800

    Add Powershell support

---
+++ Source/train.ps1
@@ -0,0 +1,8 @@
+$net='densenet169'
+$net_name='b8_lr1e-4_d0_crop'
+python -u densenet.py `
+    --batch_size 8 --epoch 0 --learning_rate 0.0001 --drop_rate 0 `
+    --load_model True --load_path ./save/$net/mura3cropfocal_1_loss.pkl `
+    --net_name $net_name `
+    --save_path ./save/$net/ `
+    > result/$net/${net_name}_+1.out
